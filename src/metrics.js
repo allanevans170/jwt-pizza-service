869ac1db3873f46e6097b957bc9ed3f7f33820f0
allanevans170 <allanevans170@gmail.com>
2024-12-11 18:06:03 -0700

    try to fix totalRequests numbers being too high?

--- src/metrics.js
+++ src/metrics.js
@@ -2,39 +2,58 @@
     const os = require('os');
 
     class Metrics {
-    constructor() {
-    this.totalRequests = 0;
-    this.getRequests = 0;
-    this.postRequests = 0;
-    this.putRequests = 0;
-    this.deleteRequests = 0;
+        constructor() {
+        this.totalRequests = 0;
+        this.getRequests = 0;
+        this.postRequests = 0;
+        this.putRequests = 0;
+        this.deleteRequests = 0;
 
-    this.authTokensCreated = 0;
-    this.usersRegistered = 0;
-    this.usersLoggedIn = 0;
-    this.unauthorizedRequests = 0;
-    this.numFranchises = 0;
-    this.numStoresCreated = 0;
-    this.menuHits = 0;
-    this.totalOrders = 0;
-    this.failedOrders = 0;
-    this.totalRevenue = 0;
+        this.authTokensCreated = 0;
+        this.usersRegistered = 0;
+        this.usersLoggedIn = 0;
+        this.unauthorizedRequests = 0;
+        this.numFranchises = 0;
+        this.numStoresCreated = 0;
+        this.menuHits = 0;
+        this.totalOrders = 0;
+        this.failedOrders = 0;
+        this.totalRevenue = 0;
 
-    // This will periodically sent metrics to Grafana
-    const timer = setInterval(() => {
-        //this.sendMetricToGrafana('auth', 'all', 'created', this.authTokensCreated);
-        //this.sendMetricToGrafana('auth', 'all', 'failed', this.unauthorizedRequests);
+        // function sendMetricsPeriodically(period) {
+        //     const timer = setInterval(() => {
+        //       try {
+        //         const buf = new MetricBuilder();
+        //         httpMetrics(buf);
+        //         systemMetrics(buf);
+        //         userMetrics(buf);
+        //         purchaseMetrics(buf);
+        //         authMetrics(buf);
 
-        this.sendMetricToGrafana('request', 'GET', 'total', this.getRequests);
-        this.sendMetricToGrafana('request', 'POST', 'total', this.postRequests);
-        this.sendMetricToGrafana('request', 'PUT', 'total', this.putRequests);
-        this.sendMetricToGrafana('request', 'DELETE', 'total', this.deleteRequests);
-        this.sendMetricToGrafana('request', 'all', 'total', this.totalRequests);
+        //         const metrics = buf.toString('\n');
+        //         this.sendMetricToGrafana(metrics);
+        //       } catch (error) {
+        //         console.log('Error sending metrics', error);
+        //       }
+        //     }, period);
+        // }
 
-        this.sendMetricToGrafana('cpu', 'all', 'usage', getCpuUsagePercentage());
-        this.sendMetricToGrafana('memory', 'all', 'usage', getMemoryUsagePercentage());
-    }, 10000);
-    timer.unref();
+
+        // This will periodically sent metrics to Grafana
+        const timer = setInterval(() => {
+            //this.sendMetricToGrafana('auth', 'all', 'created', this.authTokensCreated);
+            //this.sendMetricToGrafana('auth', 'all', 'failed', this.unauthorizedRequests);
+
+            this.sendMetricToGrafana('request', 'GET', 'total', this.getRequests);
+            this.sendMetricToGrafana('request', 'POST', 'total', this.postRequests);
+            this.sendMetricToGrafana('request', 'PUT', 'total', this.putRequests);
+            this.sendMetricToGrafana('request', 'DELETE', 'total', this.deleteRequests);
+            this.sendMetricToGrafana('request', 'all', 'total', this.totalRequests);
+
+            this.sendMetricToGrafana('cpu', 'all', 'usage', getCpuUsagePercentage());
+            this.sendMetricToGrafana('memory', 'all', 'usage', getMemoryUsagePercentage());
+        }, 10000);
+        timer.unref();
     }
 
     requestTracker(req, res, next) {
@@ -43,19 +62,15 @@
     switch(method) {
         case 'GET':
         this.incrementGetRequests();
-        this.incrementRequests();
         break;
         case 'POST':
         this.incrementPostRequests();
-        this.incrementRequests();
         break;
         case 'PUT':
         this.incrementPutRequests();
-        this.incrementRequests();
         break;
         case 'DELETE':
         this.incrementDeleteRequests();
-        this.incrementRequests();
         break;
         default:
         // Optional: handle other HTTP methods or do nothing
@@ -63,21 +78,23 @@
     }
     next();
     }
-    incrementRequests() {
-        this.totalRequests++;
-    }
     incrementGetRequests() {
         this.getRequests++;
+        this.totalRequests++;
     }
     incrementPostRequests() {
         this.postRequests++;
+        this.totalRequests++;
     }
     incrementPutRequests() {
         this.putRequests++;
+        this.totalRequests++;
     }
     incrementDeleteRequests() {
         this.deleteRequests++;
+        this.totalRequests++;
     }
+    
     incrementAuthtokensCreated() {
         this.authTokensCreated++;
     }
@@ -134,41 +151,24 @@
         }
         })
         .catch((error) => {
-        console.error('Error pushing metrics:', error);
+            console.error('Error pushing metrics:', error);
         });
     }
-    }
+}
 
-    function getCpuUsagePercentage() {
+function getCpuUsagePercentage() {
     const cpuUsage = os.loadavg()[0] / os.cpus().length;
     return cpuUsage.toFixed(2) * 100;
-    }
+}
 
-    function getMemoryUsagePercentage() {
+function getMemoryUsagePercentage() {
     const totalMemory = os.totalmem();
     const freeMemory = os.freemem();
     const usedMemory = totalMemory - freeMemory;
     const memoryUsage = (usedMemory / totalMemory) * 100;
     return memoryUsage.toFixed(2);
-    }
+}
 
-    // function sendMetricsPeriodically(period) {
-    //     const timer = setInterval(() => {
-    //       try {
-    //         const buf = new MetricBuilder();
-    //         httpMetrics(buf);
-    //         systemMetrics(buf);
-    //         userMetrics(buf);
-    //         purchaseMetrics(buf);
-    //         authMetrics(buf);
-
-    //         const metrics = buf.toString('\n');
-    //         this.sendMetricToGrafana(metrics);
-    //       } catch (error) {
-    //         console.log('Error sending metrics', error);
-    //       }
-    //     }, period);
-    // }
-
+   
     const metrics = new Metrics();
     module.exports = metrics;
